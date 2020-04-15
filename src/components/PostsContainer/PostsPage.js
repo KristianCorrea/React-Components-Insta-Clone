45a37da98aca1bbef7bbab14ff66f40430d83829
Kristian Correa <kristiangcorrea@live.com>
2020-04-15 11:30:06 -0400

    MVP

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,13 +3,21 @@
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
+import Data from '../../dummy-data';
 // import data 
 
 const PostsPage = () => {
   // set up state for your data
+  const dummyData = Data;
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {/* map through data here to return a Post and pass data as props to Post */
+        dummyData.map(function (post1){
+          return <Post post={post1} /> 
+        })
+      
+      }
+
     </div>
   );
 };
